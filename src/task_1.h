0da0952d4391a05a5de5480f5086fb40087d6483
MadinaBahriddinova <127374347+MadinaBahriddinova@users.noreply.github.com>
2023-12-04 19:22:55 +0500

    Update task_1.h

--- src/task_1.h
+++ src/task_1.h
@@ -21,3 +21,39 @@ int kthSmallest(std::vector<std::vector<int>>& matrix, int k) {
 
     return result;
 }
+
+std::vector<int> topKFrequent(std::vector<int>& nums, int k) {
+    std::unordered_map<int, int> frequencyMap;
+    
+    // Count frequency of each element
+    for (int num : nums) {
+        frequencyMap[num]++;
+    }
+
+    // Custom comparator for priority queue (min heap based on frequency)
+    auto comparator = [](std::pair<int, int>& a, std::pair<int, int>& b) {
+        return a.second > b.second;
+    };
+    
+    std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, decltype(comparator)> minHeap(comparator);
+
+    // Push elements into minHeap based on frequency
+    for (auto& entry : frequencyMap) {
+        minHeap.push(entry);
+        if (minHeap.size() > k) {
+            minHeap.pop();
+        }
+    }
+
+    // Extract top k frequent elements from minHeap
+    std::vector<int> result;
+    while (!minHeap.empty()) {
+        result.push_back(minHeap.top().first);
+        minHeap.pop();
+    }
+
+    // Reverse the result vector to get elements in descending order of frequency
+    std::reverse(result.begin(), result.end());
+
+    return result;
+}
