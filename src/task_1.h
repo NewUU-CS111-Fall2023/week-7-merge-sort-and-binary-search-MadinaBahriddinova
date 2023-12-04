b5906926a97037b2d9ef1d4b43d9f9b8e02cd800
MadinaBahriddinova <127374347+MadinaBahriddinova@users.noreply.github.com>
2023-12-04 19:10:32 +0500

    Update task_1.h

--- src/task_1.h
+++ src/task_1.h
@@ -3,3 +3,21 @@
  * Date:
  * Name:
  */
+int kthSmallest(std::vector<std::vector<int>>& matrix, int k) {
+    int n = matrix.size();
+    std::priority_queue<int, std::vector<int>, std::greater<int>> minHeap;
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            minHeap.push(matrix[i][j]);
+        }
+    }
+
+    int result = 0;
+    while (k--) {
+        result = minHeap.top();
+        minHeap.pop();
+    }
+
+    return result;
+}
